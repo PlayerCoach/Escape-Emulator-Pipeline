4868ea1bd3b00fb513bb5447501f3e5cae7641ea
Player/Coach <olafinski14@gmail.com>
2025-05-03 19:22:54 +0200

    Structurized code

--- main.py
+++ main.py
@@ -1,63 +1,89 @@
+import os
 from pywinauto.application import Application
 from pywinauto.findwindows import ElementNotFoundError
 from pywinauto import Desktop
 from pywinauto.timings import Timings
+from pywinauto.application import WindowSpecification
 import time
+
+def init_escape(escape_path:str) -> Application:
+    """
+    Initialize the Escape application. Run the Escape.exe and connect to the main window.
+    Args:
+        EscapePath (str): Path to the Escape executable. Could be a relative or absolute path.
+    Returns:
+        Application: The initialized Escape application.
+    """
+    # Check if the path is valid
+    if not os.path.exists(escape_path):
+        raise FileNotFoundError(f"Escape executable not found at {escape_path}")
+    # Start the application
+
+    try:
+        app = Application(backend="win32").start(r"..\escape32.exe")
+        app = Application(backend="win32").connect(title_re="Welcome to Escape !")
+        
+    except Exception as e:
+        print(f"Error initializing AutoIt: {e}")
+        return None
+    return app
+
+def load_config_file(config_file_path:str, _mainWin: WindowSpecification, _app: Application ) -> None:
+    """
+    Load a configuration file into the Escape application from .ecf file.
+    Args:
+        file_path (str): Path to the configuration file. Could be a relative or absolute path.
+    """
+    # Set default timeouts for this function
+    if not os.path.exists(config_file_path):
+        raise FileNotFoundError(f"Configuration file not found at {config_file_path}")
+    
+    # Load the configuration file
+    try:
+        configBtn = _mainWin.child_window(title="Configure ", class_name="TButton")
+        configBtn.click()
+        conWin = Desktop(backend="win32").window(title_re=".*[Cc]onfiguration.*")
+        conWin.menu_select("File->Open File...")
+        file_dlg = _app.window(title_re=".*Load Configuration.*")  # Adjust title if needed
+        file_dlg["Edit"].set_edit_text(config_file_path)
+        file_dlg["Open"].click()
+        conWin.close()
+    except ElementNotFoundError:
+        print("Configuration button not found (exception).")
+    except Exception as e:
+        print(f"Error loading configuration file: {e}")
+
+def load_project_file(project_file_path:str, _MAWin: WindowSpecification) -> None:
+    """
+    Load a project file into the Escape application from .mpr file.
+    Perquisite: The Escape application must be running and the Microprogrammed Architecture window must be open.
+    Args:
+        file_path (str): Path to the project file. Could be a relative or absolute path.
+    """
+    try:
+        MAWindow.menu_select("File->Open Project")
+        file_dlg = app.window(title_re=".*Load Microcode Project.*")  # Adjust title if needed
+        file_dlg["Edit"].set_edit_text(r"..\zak_simple\soi.mpr")
+        file_dlg["Open"].click()
+    except ElementNotFoundError:
+        print("Project button not found (exception).")
+    except Exception as e:
+        print(f"Error loading project file: {e}")
 
 Timings.after_clickinput_wait = 0
 Timings.after_setfocus_wait = 0
 
-start_time = time.time()
-app = Application(backend="win32").start(r"..\escape32.exe")
-app = Application(backend="win32").connect(title_re="Welcome to Escape !")
+start_tme = time.time()
+app = init_escape(r"..\escape32.exe")
+mainWin= app.window(title="Welcome to Escape !")
+load_config_file(r"..\zak_simple\soi.ecf", mainWin, app)
+mainWin.child_window(title="Microprogrammed Architecture", class_name="TButton").click()
+MAWindow = Desktop(backend="win32").window(title_re=".*Microprogrammed Architecture.*")
 
-mainWin = app.window(title="Welcome to Escape !")
+load_project_file(r"..\zak_simple\soi.mpr", MAWindow)
+    
 
-try:
-    configBtn = mainWin.child_window(title="Configure ", class_name="TButton")
-    if configBtn.exists(timeout=5):
-        print("Configure button found. Clicking...")
-        configBtn.click()
-        conWin = Desktop(backend="win32").window(title_re=".*[Cc]onfiguration.*")
-        conWin.menu_select("File->Open File...")
-        file_dlg = app.window(title_re=".*Load Configuration.*")  # Adjust title if needed
-
-        # Type the file path into the filename input
-        file_dlg["Edit"].set_edit_text(r"..\zak_simple\soi.ecf")
-
-        # Click the "Open" button
-        file_dlg["Open"].click()
-        conWin.close()
-        end_time = time.time()
-        print(f"Configuration loaded in {end_time - start_time:.2f} seconds.")
-
-        try:
-            time_start = time.time()
-            MABtn = mainWin.child_window(title="Microprogrammed Architecture", class_name="TButton")
-            MABtn.click()
-            MAWindow = Desktop(backend="win32").window(title_re=".*Microprogrammed Architecture.*")
-            MAWindow.menu_select("File->Open Project")
-            file_dlg = app.window(title_re=".*Load Microcode Project.*")  # Adjust title if needed
-            file_dlg["Edit"].set_edit_text(r"..\zak_simple\soi.mpr")
-            file_dlg["Open"].click()
-            # save result to a file 
-            time_field = MAWindow.child_window(class_name="TEditInteger")
-            print(time_field.window_text())
-            time_field.set_edit_text("2137")  # or whatever input format is expected
-            print(time_field.window_text())
-            time_end = time.time()
-            print(f"Microprogrammed Architecture loaded in {time_end - time_start:.2f} seconds.")
-
-
-        except ElementNotFoundError:
-            print("Microprogrammed Architecture button not found (exception).")
-
-        
-        
-    else:
-        print("Configure button not found.")
-except ElementNotFoundError:
-    print("Configure button not found (exception).")
-
-# win.print_control_identifiers(depth=3)
-
+# # win.print_control_identifiers(depth=3)
+#    time_field = MAWindow.child_window(class_name="TEditInteger")
+#     print(time_field.window_text())
+#     time_field.set_edit_text("2137")  # or whatever input format is expected
